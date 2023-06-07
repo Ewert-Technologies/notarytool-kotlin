b03b92e9bbe508f1cc89a0378fcf720568889449
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-07 12:35:09 -0700

    Re-organized build files
    Added project name

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -19,23 +19,24 @@ plugins {
 
 // Repositories for Library Dependencies
 repositories {
-  // Use Maven Central for resolving dependencies.
   mavenCentral()
 }
 
 // Library Dependencies
 dependencies {
+
   // These dependencies are exported to consumers, that is to say found on their compile classpath.
-  api("org.apache.commons:commons-math3:3.6.1")
+//  api("org.apache.commons:commons-math3:3.6.1")
 
   // These dependencies are used internally, and not exposed to consumers on their own compile classpath.
-  implementation("com.google.guava:guava:31.1-jre")
+//  implementation("com.google.guava:guava:31.1-jre")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
   toolchain {
     languageVersion.set(JavaLanguageVersion.of(17))
+    vendor.set(JvmVendorSpec.AZUL)
   }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,10 @@
  */
 
 plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+  // Apply the foojay-resolver plugin to allow automatic download of JDKs
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
-rootProject.name = "notarytool-kotlin"
+rootProject.name = "notarytool-kotlin-root"
 include("lib")
+project(file("./lib")).name = "notarytool-kotlin"
