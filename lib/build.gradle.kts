4dd9377108acc7f9d0b663bb3e61b005b92c97a5
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-07 11:17:58 -0700

    Cleaned up and re-organized build script

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,40 +7,46 @@
  * This project uses @Incubating APIs which are subject to change.
  */
 
+//
+// Plugins
+//
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
-
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+  java
+  idea
+  `java-library`
+  id("org.jetbrains.kotlin.jvm") version "1.8.21"
 }
 
+// Repositories for Library Dependencies
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
+// Library Dependencies
 dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+  // These dependencies are exported to consumers, that is to say found on their compile classpath.
+  api("org.apache.commons:commons-math3:3.6.1")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
-}
-
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("1.8.21")
-        }
-    }
+  // These dependencies are used internally, and not exposed to consumers on their own compile classpath.
+  implementation("com.google.guava:guava:31.1-jre")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
 }
+
+testing {
+  suites {
+    // Configure the built-in test suite
+    val test by getting(JvmTestSuite::class) {
+      // Use Kotlin Test test framework
+      useKotlinTest("1.8.21")
+    }
+  }
+}
+
+
