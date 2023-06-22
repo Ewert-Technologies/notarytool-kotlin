b316a1c0422f2953ecdf8bbcce368889ea453f96
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-22 14:43:56 -0700

    Improved Error classes

--- lib/src/main/kotlin/ca/ewert/notarytoolkotlin/errors/Errors.kt
+++ lib/src/main/kotlin/ca/ewert/notarytoolkotlin/errors/Errors.kt
@@ -7,13 +7,22 @@ package ca.ewert.notarytoolkotlin.errors
  */
 sealed interface NotaryToolError {
   val msg: String
+
+  /**
+   * @property msg The error message
+   * @property jsonString The json String that was used when attempting to parse
+   */
+  data class JsonParseError(override val msg: String, val jsonString: String?) : NotaryToolError
+
+  data class GeneralError(override val msg: String) : NotaryToolError
+
+  data class HttpError(override val msg: String, val httpStatusCode: Int, val httpStatusMsg: String, val requestUrl: String = "") : NotaryToolError
 }
 
 sealed interface JsonWebTokenError : NotaryToolError {
-  class PrivateKeyNotFound(override val msg: String) : JsonWebTokenError
-  class TokenCreationError(override val msg: String) : JsonWebTokenError
+  data class PrivateKeyNotFound(override val msg: String) : JsonWebTokenError
+  data class TokenCreationError(override val msg: String) : JsonWebTokenError
 }
 
-sealed class JsonParseError(override val msg: String) : NotaryToolError
 
-sealed class HttpError(override  val msg: String, val httpStatusCode: Int, val httpStatusMsg: String) : NotaryToolError
+
