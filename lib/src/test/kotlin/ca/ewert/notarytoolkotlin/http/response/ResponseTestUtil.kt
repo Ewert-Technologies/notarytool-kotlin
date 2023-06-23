11c4c9ab3d33283c564aad998eefc48001cddf1c
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-23 10:32:12 -0700

    Added function for creating a 401 error response

--- lib/src/test/kotlin/ca/ewert/notarytoolkotlin/http/response/ResponseTestUtil.kt
+++ lib/src/test/kotlin/ca/ewert/notarytoolkotlin/http/response/ResponseTestUtil.kt
@@ -14,7 +14,10 @@ private val log = KotlinLogging.logger {}
  * @author vewert
  */
 
-fun createMockResponse(body: String): MockResponse {
+/**
+ * Creates a [MockResponse] with a status of 200 and all headers of an expected Response
+ */
+fun createMockResponse200(body: String): MockResponse {
   val headerDateString = ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("GMT"))
     .format(NotaryApiResponse.HTTP_DATE_TIME)
   log.info {"Header Date String: $headerDateString" }
@@ -26,4 +29,25 @@ fun createMockResponse(body: String): MockResponse {
     .addHeader("Connection", "keep-alive")
     .addHeader("Set-Cookie", "dqsid=; Expires=Thu, 01 Jan 1970 00")
     .setBody(body)
+}
+
+/**
+ * Creates a [MockResponse] with a status of 401 with headers and body matching Apples response.
+ */
+fun createMockResponse401(): MockResponse {
+  val headerDateString = ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("GMT"))
+    .format(NotaryApiResponse.HTTP_DATE_TIME)
+  val content = """
+    Unauthenticated
+
+    Request ID: TYPN6E62TY76LDJXYZTMLQRK7I.0.0
+  """.trimIndent()
+  log.info {"Header Date String: $headerDateString" }
+  return MockResponse().setResponseCode(401)
+    .addHeader("Server", "daiquiri/3.0.0")
+    .addHeader("Content-Type", "text/plain")
+    .addHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+    .addHeader("Date", headerDateString)
+    .addHeader("Connection", "close")
+    .setBody(content)
 }
