0066be8e6c3743a3bc885f0441062890c22941c0
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-29 10:25:38 -0700

    Added ErrorResponseJson Types and tests

---
+++ lib/src/main/kotlin/ca/ewert/notarytoolkotlin/http/json/notaryapi/ErrorResponseJson.kt
@@ -0,0 +1,71 @@
+package ca.ewert.notarytoolkotlin.http.json.notaryapi
+
+import ca.ewert.notarytoolkotlin.errors.NotaryToolError
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonDataException
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+private val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+
+/**
+ * Top-level Response when there is an error.
+ *
+ * Instance can be created by parsing json response
+ *
+ * @property errors A list of the [Error] responses.
+ * @author Victor Ewert
+ */
+data class ErrorResponseJson(
+  val errors: List<Error>,
+) {
+  companion object {
+
+    /**
+     * Creates a [ErrorResponseJson] from the json String
+     *
+     * @param jsonString The json String to parse
+     * @return A [ErrorResponseJson] or a [NotaryToolError.JsonParseError]
+     */
+    @JvmStatic
+    fun create(jsonString: String?): Result<ErrorResponseJson, NotaryToolError.JsonParseError> {
+      return if (!jsonString.isNullOrEmpty()) {
+        val jsonAdapter: JsonAdapter<ErrorResponseJson> =
+          moshi.adapter(ErrorResponseJson::class.java).failOnUnknown().lenient()
+        try {
+          val errorResponseJson: ErrorResponseJson? = jsonAdapter.fromJson(jsonString)
+          if (errorResponseJson != null) {
+            Ok(errorResponseJson)
+          } else {
+            Err(NotaryToolError.JsonParseError("Error creating Json Object.", jsonString))
+          }
+        } catch (jsonDataException: JsonDataException) {
+          Err(NotaryToolError.JsonParseError("Error parsing json: ${jsonDataException.message}.", jsonString))
+        }
+      } else {
+        Err(NotaryToolError.JsonParseError("Json String is <null> or empty.", jsonString = jsonString))
+      }
+    }
+  }
+}
+
+/**
+ * Error data from the Response.
+ *
+ * @property code The HTTP response code message.
+ * @property detail Details of the error.
+ * @property id The submissionId used in the request.
+ * @property status The HTTP status code.
+ * @property title The error message title.
+ * @author Victor Ewert
+ */
+data class Error(
+  val code: String,
+  val detail: String,
+  val id: String,
+  val status: String,
+  val title: String,
+)
---
+++ lib/src/test/kotlin/ca/ewert/notarytoolkotlin/http/json/notaryapi/ErrorResponseJsonTests.kt
@@ -0,0 +1,138 @@
+package ca.ewert.notarytoolkotlin.http.json.notaryapi
+
+import assertk.assertThat
+import assertk.assertions.hasSize
+import assertk.assertions.isEqualTo
+import assertk.assertions.prop
+import ca.ewert.notarytoolkotlin.errors.NotaryToolError
+import ca.ewert.notarytoolkotlin.isErr
+import ca.ewert.notarytoolkotlin.isOk
+import com.github.michaelbull.result.getError
+import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+/** Logging Object */
+private val log = KotlinLogging.logger {}
+
+/**
+ * Unit Tests for [ErrorResponseJson]
+ *
+ * @author Victor Ewert
+ */
+class ErrorResponseJsonTests {
+
+  /**
+   * Basic test that verifies correct json can be parsed
+   */
+  @Test
+  @DisplayName("Basic Test")
+  fun createTest1() {
+    val jsonString = """
+    {
+      "errors": [{
+        "id": "228afb9e-58fa-4246-8fed-c0dec1f23595",
+        "status": "404",
+        "code": "NOT_FOUND",
+        "title": "The specified resource does not exist",
+        "detail": "There is no resource of type 'submissions' with id '5685647e-0125-4343-a068-1c5786499827'"
+      }]
+    }
+    """.trimIndent()
+
+    val errorResponseJsonResult = ErrorResponseJson.create(jsonString)
+    assertThat(errorResponseJsonResult).isOk()
+
+    errorResponseJsonResult.onSuccess { errorResponseJson ->
+      assertThat(errorResponseJson.errors).hasSize(1)
+      assertThat(errorResponseJson.errors[0]).prop(Error::id).isEqualTo("228afb9e-58fa-4246-8fed-c0dec1f23595")
+      assertThat(errorResponseJson.errors[0]).prop(Error::status).isEqualTo("404")
+      assertThat(errorResponseJson.errors[0]).prop(Error::code).isEqualTo("NOT_FOUND")
+      assertThat(errorResponseJson.errors[0]).prop(Error::title).isEqualTo("The specified resource does not exist")
+      assertThat(errorResponseJson.errors[0]).prop(Error::detail)
+        .isEqualTo("There is no resource of type 'submissions' with id '5685647e-0125-4343-a068-1c5786499827'")
+    }
+  }
+
+  /**
+   * Attempts to create a [ErrorResponseJson] from a jsonString that is `null`.
+   * Verifies that an Error is returned
+   */
+  @Test
+  @DisplayName("null jsonString Test")
+  fun createTest2() {
+    val jsonString: String? = null
+    val errorResponseJsonResult = ErrorResponseJson.create(jsonString)
+    assertThat(errorResponseJsonResult).isErr()
+    errorResponseJsonResult.onFailure { jsonParseError ->
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::msg)
+        .isEqualTo("Json String is <null> or empty.")
+    }
+  }
+
+  /**
+   * Attempts to create a [ErrorResponseJson] from a jsonString that is empty.
+   * Verifies that an Error is returned
+   */
+  @Test
+  @DisplayName("empty jsonString Test")
+  fun createTest3() {
+    val jsonString = ""
+    val errorResponseJsonResult = ErrorResponseJson.create(jsonString)
+    assertThat(errorResponseJsonResult).isErr()
+    errorResponseJsonResult.onFailure { jsonParseError ->
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::msg)
+        .isEqualTo("Json String is <null> or empty.")
+    }
+  }
+
+  /**
+   * Attempts to create a [ErrorResponseJson] from a jsonString that is valid json but not the
+   * expected json.
+   * Verifies that an Error is returned
+   */
+  @Test
+  @DisplayName("unexpected jsonString Test")
+  fun createTest4() {
+    val jsonString = """
+    {
+      "errors": [{
+        "id": "228afb9e-58fa-4246-8fed-c0dec1f23595",
+        "status_code": "404",
+        "status_message": "NOT_FOUND",
+        "title": "The specified resource does not exist",
+        "detail": "There is no resource of type 'submissions' with id '5685647e-0125-4343-a068-1c5786499827'"
+      }]
+    }
+    """.trimIndent()
+    val errorResponseJsonResult = ErrorResponseJson.create(jsonString)
+    assertThat(errorResponseJsonResult).isErr()
+    errorResponseJsonResult.onFailure { jsonParseError ->
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::msg)
+        .isEqualTo("Error parsing json: Cannot skip unexpected NAME at \$.errors[0].status_code.")
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::jsonString)
+        .isEqualTo(jsonString)
+    }
+  }
+
+  /**
+   * Attempts to create a [ErrorResponseJson] from a jsonString contains invalid json
+   * Verifies that an Error is returned
+   */
+  @Test
+  @DisplayName("invalid jsonString Test")
+  fun createTest5() {
+    val jsonString = "I am Jason!"
+    val errorResponseJsonResult = ErrorResponseJson.create(jsonString)
+    assertThat(errorResponseJsonResult).isErr()
+    errorResponseJsonResult.onFailure { jsonParseError ->
+      log.info(errorResponseJsonResult.getError().toString())
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::msg)
+        .isEqualTo("Error parsing json: Expected BEGIN_OBJECT but was STRING at path \$.")
+      assertThat(jsonParseError).prop(NotaryToolError.JsonParseError::jsonString)
+        .isEqualTo(jsonString)
+    }
+  }
+}
