7139ce4cac808c2552f3e05adc4e0a3a0d054f79
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-06-28 18:46:39 -0700

    Added more tests

--- lib/src/test/kotlin/ca/ewert/notarytoolkotlin/NotaryToolClientTests.kt
+++ lib/src/test/kotlin/ca/ewert/notarytoolkotlin/NotaryToolClientTests.kt
@@ -54,10 +54,13 @@ class NotaryToolClientTests {
     mockWebServer.shutdown()
   }
 
+  /**
+   * Tests making a request to getPreviousSubmission, with a valid response.
+   */
   @Test
   @Tag("MockServer")
   @DisplayName("getPreviousSubmissions - Success Test")
-  fun test1() {
+  fun getPreviousSubmissionsTest1() {
     val responseBody: String = """
     {
       "data": [
@@ -129,10 +132,13 @@ class NotaryToolClientTests {
     }
   }
 
+  /**
+   * Tests making a request to getPreviousSubmission, with a 401 - Unauthenticated
+   */
   @Test
   @Tag("MockServer")
   @DisplayName("getPreviousSubmissions - 401 Test")
-  fun test2() {
+  fun getPreviousSubmissionTest2() {
     mockWebServer.enqueue(createMockResponse401())
 
     mockWebServer.start()
@@ -152,18 +158,129 @@ class NotaryToolClientTests {
     getPreviousSubmissionsResult.onFailure { error ->
       log.info { "onFailure(): $error" }
       when (error) {
-        is JsonWebTokenError.TokenCreationError -> log.warn { error.msg }
-        is NotaryToolError.HttpError -> {
-          log.warn {
-            "An HTTP Error occurred. " +
-              "Code: ${error.httpStatusCode} - ${error.httpStatusMsg}, " +
-              "for request to: ${error.requestUrl}. Response Body: '${error.contentBody}'"
-          }
-          assertThat(error.httpStatusCode).isEqualTo(401)
+        is JsonWebTokenError.AuthenticationError -> {
+          log.info { "Authentication Error" }
+          assertThat(error.msg).isEqualTo("Web Service could not authenticate the request.")
         }
 
-        else -> log.warn { error.msg }
+        else -> log.warn {
+          error.msg
+          fail(AssertionError("Incorrect Error: $error"))
+        }
       }
+    }
+  }
+
+  /**
+   * Tests making a request to getPreviousSubmission, with unexpected json in the response body.
+   */
+  @Test
+  @Tag("MockServer")
+  @DisplayName("getPreviousSubmissions - Invalid json")
+  fun getPreviousSubmissionsTest3() {
+    val responseBody: String = """
+    {
+      "datas": [
+        {
+          "attributes": {
+            "createdDate": "2021-04-29T01:38:09.498Z",
+            "name": "OvernightTextEditor_11.6.8.zip",
+            "status": "Accepted"
+          },
+          "id": "2efe2717-52ef-43a5-96dc-0797e4ca1041",
+          "type": "submissions"
+        },
+        {
+          "attributes": {
+            "createdDate": "2021-04-23T17:44:54.761Z",
+            "name": "OvernightTextEditor_11.6.7.zip",
+            "status": "Accepted"
+          },
+          "id": "cf0c235a-dad2-4c24-96eb-c876d4cb3a2d",
+          "type": "submissions"
+        },
+        {
+          "attributes": {
+            "createdDate": "2021-04-19T16:56:17.839Z",
+            "name": "OvernightTextEditor_11.6.7.zip",
+            "status": "Invalid"
+          },
+          "id": "38ce81cc-0bf7-454b-91ef-3f7395bf297b",
+          "type": "submissions"
+        }
+      ],
+      "meta": {}
+    }
+    """.trimIndent()
+
+    mockWebServer.enqueue(createMockResponse200(responseBody))
+
+    mockWebServer.start()
+    val baseUrl: HttpUrl = mockWebServer.url("")
+
+    val notaryToolClient = NotaryToolClient(
+      privateKeyId = "A8B3X24VG1",
+      issuerId = "70a7de6a-a537-48e3-a053-5a8a7c22a4a1",
+      privateKeyFile = privateKeyFile!!,
+      baseUrlString = baseUrl.toString(),
+    )
+
+    val getPreviousSubmissionsResult = notaryToolClient.getPreviousSubmissions()
+    assertThat(getPreviousSubmissionsResult).isErr()
+
+    getPreviousSubmissionsResult.onFailure { error ->
+      when (error) {
+        is NotaryToolError.JsonParseError -> {
+          log.info { error }
+          assertThat(error.msg).isEqualTo("Error parsing json: Cannot skip unexpected NAME at \$.datas.")
+          assertThat(error.jsonString).isEqualTo(responseBody)
+        }
+
+        else -> {
+          log.warn { error.msg }
+          fail(AssertionError("Incorrect Error: $error"))
+        }
+      }
+    }
+  }
+
+  /**
+   * Tests making a request to getPreviousSubmission, with a valid response, but no
+   * submission data.
+   */
+  @Test
+  @Tag("MockServer")
+  @DisplayName("getPreviousSubmissions - No data")
+  fun getPreviousSubmissionsTest4() {
+    val responseBody: String = """
+    {
+      "data": [
+      ],
+      "meta": {}
+    }
+    """.trimIndent()
+
+    mockWebServer.enqueue(createMockResponse200(responseBody))
+
+    mockWebServer.start()
+    val baseUrl: HttpUrl = mockWebServer.url("")
+
+    val notaryToolClient = NotaryToolClient(
+      privateKeyId = "A8B3X24VG1",
+      issuerId = "70a7de6a-a537-48e3-a053-5a8a7c22a4a1",
+      privateKeyFile = privateKeyFile!!,
+      baseUrlString = baseUrl.toString(),
+    )
+
+    val getPreviousSubmissionsResult = notaryToolClient.getPreviousSubmissions()
+    assertThat(getPreviousSubmissionsResult).isOk()
+
+    getPreviousSubmissionsResult.onSuccess { submissionListResponse ->
+      assertThat(submissionListResponse.submissionInfoList).hasSize(0)
+    }
+
+    getPreviousSubmissionsResult.onFailure { error ->
+      fail(AssertionError("Request failed with: $error"))
     }
   }
 }
