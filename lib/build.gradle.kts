9005862027c68a5e381823e1ba78afb5b28dcd0b
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-08-17 09:32:02 -0700

    Added plugin to generate third-party license summaries

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -30,6 +30,7 @@ plugins {
   idea
   `java-library`
   `maven-publish`
+  id("com.jaredsburrows.license") version "0.9.3"
   id("org.jetbrains.kotlin.jvm") version "1.9.0"
   id("com.github.ben-manes.versions") version "0.47.0"
   id("org.barfuin.gradle.taskinfo") version "2.1.0"
