971f45a83a6fc10a5adc28cd8272806feba43b2f
Victor Ewert <victor.ewert@ewert-technologies.ca>
2023-10-10 17:19:50 -0700

    Updated more dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm") version "1.9.0"
-  id("org.jetbrains.dokka") version "1.8.20"
-  id("org.jmailen.kotlinter") version "3.15.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.dokka") version "1.9.0"
+    id("org.jmailen.kotlinter") version "3.15.0" apply false
 }
 
 subprojects {
-  apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "org.jetbrains.dokka")
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
